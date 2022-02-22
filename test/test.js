5c14388d3375b61640f29961ff0b29816f47a681
XhmikosR <xhmikosr@gmail.com>
2022-02-22 13:14:28 +0200

    Update all the things (#19)
    * add .editorconfig
    * enforce LF
    * add ESLint
    * assorted lint tweaks
    * switch to GitHub Actions
    * add LICENSE
    * require Node.js >= 6

--- test/test.js
+++ test/test.js
@@ -1,198 +1,200 @@
-var types = require('../');
-var assert = require('assert');
-var util = require('util');
-var Walker = require('node-source-walk');
+/* eslint-env mocha */
 
-describe('module-types', function() {
-  // Checks whether of not the checker succeeds on
-  // a node in the AST of the given source code
-  function check(code, checker, harmony) {
-    var found = false;
-    var walker = new Walker({
-      esprimaHarmony: !!harmony
-    });
+'use strict';
 
-    walker.walk(code, function(node) {
-      // Use call to avoid .bind(types) everywhere
-      if (checker.call(types, node)) {
-        found = true;
-        walker.stopWalking();
-      }
-    });
+// TODO switch to assert.strict
+const assert = require('assert');
+const Walker = require('node-source-walk');
+const types = require('../index.js');
 
-    return found;
-  }
+// Checks whether of not the checker succeeds on
+// a node in the AST of the given source code
+function check(code, checker, harmony) {
+  let found = false;
+  const walker = new Walker({ esprimaHarmony: Boolean(harmony) });
 
-  describe('isDefine', function() {
-    it('detects define function calls', function() {
-      assert(check('define();', types.isDefine));
+  walker.walk(code, (node) => {
+    // Use call to avoid .bind(types) everywhere
+    if (checker.call(types, node)) {
+      found = true;
+      walker.stopWalking();
+    }
+  });
+
+  return found;
+}
+  
+describe('module-types', () => {
+  describe('isDefine', () => {
+    it('detects define function calls', () => {
+      assert.ok(check('define();', types.isDefine));
     });
   });
 
-  describe('isDefineAMD', function() {
-    it('does not detect a generic define function call', function() {
-      assert(!check('define();', types.isDefineAMD));
+  describe('isDefineAMD', () => {
+    it('does not detect a generic define function call', () => {
+      assert.ok(!check('define();', types.isDefineAMD));
       // Named form
-      assert(check('define("foobar", ["a"], function(a){});', types.isDefineAMD));
+      assert.ok(check('define("foobar", ["a"], function(a){});', types.isDefineAMD));
       // Dependency form
-      assert(check('define(["a"], function(a){});', types.isDefineAMD));
+      assert.ok(check('define(["a"], function(a){});', types.isDefineAMD));
       // Factory form
-      assert(check('define(function(require){});', types.isDefineAMD));
+      assert.ok(check('define(function(require){});', types.isDefineAMD));
       // REM form
-      assert(check('define(function(require, exports, module){});', types.isDefineAMD));
+      assert.ok(check('define(function(require, exports, module){});', types.isDefineAMD));
       // No-dependency form
-      assert(check('define({});', types.isDefineAMD));
+      assert.ok(check('define({});', types.isDefineAMD));
     });
 
-    it('detects a named form AMD define function call', function() {
-      assert(!check('define();', types.isDefineAMD));
-    });
-  });
-
-  describe('isRequire', function() {
-    it('detects require function calls', function() {
-      assert(check('require();', types.isRequire));
+    it('detects a named form AMD define function call', () => {
+      assert.ok(!check('define();', types.isDefineAMD));
     });
   });
 
-  describe('isRequire with main-scoped require ', function() {
-    it('detects require function calls', function() {
-      assert(check('require.main.require();', types.isRequire));
+  describe('isRequire', () => {
+    it('detects require function calls', () => {
+      assert.ok(check('require();', types.isRequire));
     });
   });
 
-  describe('isTopLevelRequire', function() {
-    it('detects top-level (i.e., top of file) require function calls', function() {
-      assert(check('require();', types.isTopLevelRequire));
-      assert(!check('var foo = 2; \nrequire([], function(){});', types.isTopLevelRequire));
-      assert(check('require(["a"], function(a){});', types.isTopLevelRequire));
+  describe('isRequire with main-scoped require ', () => {
+    it('detects require function calls', () => {
+      assert.ok(check('require.main.require();', types.isRequire));
+    });
+  });
+
+  describe('isTopLevelRequire', () => {
+    it('detects top-level (i.e., top of file) require function calls', () => {
+      assert.ok(check('require();', types.isTopLevelRequire));
+      assert.ok(!check('var foo = 2; \nrequire([], function(){});', types.isTopLevelRequire));
+      assert.ok(check('require(["a"], function(a){});', types.isTopLevelRequire));
     });
 
-    it('does not fail on es6', function() {
-      assert.doesNotThrow(function() {
+    it('does not fail on es6', () => {
+      assert.doesNotThrow(() => {
         check('import require from "mylib"; \nrequire();', types.isTopLevelRequire, true);
       });
     });
   });
 
-  describe('isExports', function() {
-    it('detects module.exports CJS style exports', function() {
-      assert(check('module.exports.foo = function() {};', types.isExports));
-      assert(check('module.exports = function() {};', types.isExports));
-      assert(check('module.exports = {};', types.isExports));
+  describe('isExports', () => {
+    it('detects module.exports CJS style exports', () => {
+      assert.ok(check('module.exports.foo = function() {};', types.isExports));
+      assert.ok(check('module.exports = function() {};', types.isExports));
+      assert.ok(check('module.exports = {};', types.isExports));
     });
 
-    it('detects plain exports CJS style exports', function() {
-      assert(check('exports = function() {};', types.isExports));
-      assert(check('exports.foo = function() {};', types.isExports));
-      assert(check('exports = {};', types.isExports));
-    });
-  });
-
-  describe('AMD modules', function() {
-    it('detects driver scripts', function() {
-      assert(check('require(["a"], function(a){});', types.isAMDDriverScriptRequire));
-    });
-
-    it('does not get confused with a commonjs require', function() {
-      assert(!check('require("foo");', types.isAMDDriverScriptRequire));
-    });
-
-    describe('named form', function() {
-      it('detects named form', function() {
-        assert(check('define("foobar", ["a"], function(a){});', types.isNamedForm));
-      });
-
-      it('needs 3 arguments', function() {
-        assert(!check('define("foobar", ["a"]);', types.isNamedForm));
-        assert(!check('define("foobar", ["a"], function(a){}, "foo");', types.isNamedForm));
-      });
-
-      it('needs the first argument to be a literal', function() {
-        assert(!check('define(["foobar"], ["a"], function(a){});', types.isNamedForm));
-      });
-
-      it('needs the second argument to be an array', function() {
-        assert(!check('define("foobar", 123, function(a){});', types.isNamedForm));
-      });
-
-      it('needs the third argument to be a function', function() {
-        assert(!check('define("foobar", ["foo"], 123);', types.isNamedForm));
-        assert(!check('define("reset", [0, 0], "modifier");', types.isNamedForm));
-      });
-    });
-
-    describe('dependency form', function() {
-      it('detects dependency form modules', function() {
-        assert(check('define(["a"], function(a){});', types.isDependencyForm));
-      });
-
-      it('needs the first argument to be an array', function() {
-        assert(!check('define(123, function(a){});', types.isDependencyForm));
-      });
-
-      it('needs the second argument to be a function', function() {
-        assert(!check('define(["a"], 123);', types.isDependencyForm));
-      });
-
-      it('needs 2 arguments', function() {
-        assert(!check('define(["a"], function(a){}, 123);', types.isDependencyForm));
-      });
-    });
-
-    describe('factory form', function() {
-      it('detects factory form modules', function() {
-        assert(check('define(function(require){});', types.isFactoryForm));
-      });
-
-      it('needs one argument', function() {
-        assert(!check('define(function(require){}, 123);', types.isFactoryForm));
-      });
-
-      it('needs the first argument to be a function', function() {
-        assert(!check('define(123);', types.isFactoryForm));
-      });
-    });
-
-    it('detects REM form modules', function() {
-      assert(check('define(function(require, exports, module){});', types.isREMForm));
-    });
-
-    describe('no dependency form', function() {
-      it('detects no dependency form modules', function() {
-        assert(check('define({});', types.isNoDependencyForm));
-      });
-
-      it('needs a aingle argument', function() {
-        assert(!check('define({}, 123);', types.isNoDependencyForm));
-      });
-
-      it('needs the first argument to be an object', function() {
-        assert(!check('define(function(){});', types.isNoDependencyForm));
-      });
+    it('detects plain exports CJS style exports', () => {
+      assert.ok(check('exports = function() {};', types.isExports));
+      assert.ok(check('exports.foo = function() {};', types.isExports));
+      assert.ok(check('exports = {};', types.isExports));
     });
   });
 
-  describe('ES6', function() {
-    it('detects es6 imports', function() {
-      assert(check('import {foo, bar} from "mylib";', types.isES6Import, true));
-      assert(check('import * as foo from "mod.js";', types.isES6Import, true));
-      assert(check('import "mylib2";', types.isES6Import, true));
-      assert(check('import foo from "mod.js";', types.isES6Import, true));
-      assert(check('import("foo");', types.isES6Import, true));
+  describe('AMD modules', () => {
+    it('detects driver scripts', () => {
+      assert.ok(check('require(["a"], function(a){});', types.isAMDDriverScriptRequire));
     });
 
-    it('detects es6 exports', function () {
-      assert(check('export default 123;', types.isES6Export, true));
-      assert(check('export {foo, bar}; function foo() {} function bar() {}', types.isES6Export, true));
-      assert(check('export { D as default }; class D {}', types.isES6Export, true));
-      assert(check('export function inc() { counter++; }', types.isES6Export, true));
-      assert(check('export * from "mod";', types.isES6Export, true));
+    it('does not get confused with a commonjs require', () => {
+      assert.ok(!check('require("foo");', types.isAMDDriverScriptRequire));
     });
 
-    it('detects dynamic imports', function() {
-      assert(check('import("./bar");', types.isDynamicImport, true));
-      assert(check('function foo() { import("./bar"); }', types.isDynamicImport, true));
+    describe('named form', () => {
+      it('detects named form', () => {
+        assert.ok(check('define("foobar", ["a"], function(a){});', types.isNamedForm));
+      });
+
+      it('needs 3 arguments', () => {
+        assert.ok(!check('define("foobar", ["a"]);', types.isNamedForm));
+        assert.ok(!check('define("foobar", ["a"], function(a){}, "foo");', types.isNamedForm));
+      });
+
+      it('needs the first argument to be a literal', () => {
+        assert.ok(!check('define(["foobar"], ["a"], function(a){});', types.isNamedForm));
+      });
+
+      it('needs the second argument to be an array', () => {
+        assert.ok(!check('define("foobar", 123, function(a){});', types.isNamedForm));
+      });
+
+      it('needs the third argument to be a function', () => {
+        assert.ok(!check('define("foobar", ["foo"], 123);', types.isNamedForm));
+        assert.ok(!check('define("reset", [0, 0], "modifier");', types.isNamedForm));
+      });
+    });
+
+    describe('dependency form', () => {
+      it('detects dependency form modules', () => {
+        assert.ok(check('define(["a"], function(a){});', types.isDependencyForm));
+      });
+
+      it('needs the first argument to be an array', () => {
+        assert.ok(!check('define(123, function(a){});', types.isDependencyForm));
+      });
+
+      it('needs the second argument to be a function', () => {
+        assert.ok(!check('define(["a"], 123);', types.isDependencyForm));
+      });
+
+      it('needs 2 arguments', () => {
+        assert.ok(!check('define(["a"], function(a){}, 123);', types.isDependencyForm));
+      });
+    });
+
+    describe('factory form', () => {
+      it('detects factory form modules', () => {
+        assert.ok(check('define(function(require){});', types.isFactoryForm));
+      });
+
+      it('needs one argument', () => {
+        assert.ok(!check('define(function(require){}, 123);', types.isFactoryForm));
+      });
+
+      it('needs the first argument to be a function', () => {
+        assert.ok(!check('define(123);', types.isFactoryForm));
+      });
+    });
+
+    it('detects REM form modules', () => {
+      assert.ok(check('define(function(require, exports, module){});', types.isREMForm));
+    });
+
+    describe('no dependency form', () => {
+      it('detects no dependency form modules', () => {
+        assert.ok(check('define({});', types.isNoDependencyForm));
+      });
+
+      it('needs a aingle argument', () => {
+        assert.ok(!check('define({}, 123);', types.isNoDependencyForm));
+      });
+
+      it('needs the first argument to be an object', () => {
+        assert.ok(!check('define(function(){});', types.isNoDependencyForm));
+      });
+    });
+  });
+
+  describe('ES6', () => {
+    it('detects es6 imports', () => {
+      assert.ok(check('import {foo, bar} from "mylib";', types.isES6Import, true));
+      assert.ok(check('import * as foo from "mod.js";', types.isES6Import, true));
+      assert.ok(check('import "mylib2";', types.isES6Import, true));
+      assert.ok(check('import foo from "mod.js";', types.isES6Import, true));
+      assert.ok(check('import("foo");', types.isES6Import, true));
+    });
+
+    it('detects es6 exports', () => {
+      assert.ok(check('export default 123;', types.isES6Export, true));
+      assert.ok(check('export {foo, bar}; function foo() {} function bar() {}', types.isES6Export, true));
+      assert.ok(check('export { D as default }; class D {}', types.isES6Export, true));
+      assert.ok(check('export function inc() { counter++; }', types.isES6Export, true));
+      assert.ok(check('export * from "mod";', types.isES6Export, true));
+    });
+
+    it('detects dynamic imports', () => {
+      assert.ok(check('import("./bar");', types.isDynamicImport, true));
+      assert.ok(check('function foo() { import("./bar"); }', types.isDynamicImport, true));
     });
   });
 });
