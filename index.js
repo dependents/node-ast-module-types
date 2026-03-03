c4e4d1d9fb8286fb7b92ccb4b14767d022ddce4e
XhmikosR <xhmikosr@gmail.com>
2026-03-03 22:01:40 +0200

    Convert to a class

--- index.js
+++ index.js
@@ -1,236 +1,237 @@
 'use strict';
 
-// Deprecated
-// Whether or not the node represents a generic define() call
-// Note: this should not be used as it will have false positives.
-// It is mostly used to guide sniffs for other methods and will be made private eventually.
-module.exports.isDefine = function(node) {
-  if (!node) return false;
+class AstModuleTypes {
+  // Deprecated
+  // Whether or not the node represents a generic define() call
+  // Note: this should not be used as it will have false positives.
+  // It is mostly used to guide sniffs for other methods and will be made private eventually.
+  isDefine(node) {
+    if (!node) return false;
 
-  const c = node.callee;
+    const c = node.callee;
 
-  return c &&
-         node.type === 'CallExpression' &&
-         c.type === 'Identifier' &&
-         c.name === 'define';
-};
-
-// Whether or not the node represents any of the AMD define() forms
-module.exports.isDefineAMD = function(node) {
-  if (!node) return false;
-
-  const e = module.exports;
-
-  return e.isNamedForm(node) || e.isDependencyForm(node) ||
-         e.isFactoryForm(node) || e.isNoDependencyForm(node) ||
-         e.isREMForm(node);
-};
-
-// Whether or not the node represents a require function call
-module.exports.isRequire = function(node) {
-  return this.isPlainRequire(node) || this.isMainScopedRequire(node);
-};
-
-// Whether or not the node represents a plain require function call [require(...)]
-module.exports.isPlainRequire = function(node) {
-  if (!node) return false;
-
-  const c = node.callee;
-
-  return c &&
-         node.type === 'CallExpression' &&
-         c.type === 'Identifier' &&
-         c.name === 'require';
-};
-
-// Whether or not the node represents main-scoped require function call [require.main.require(...)]
-module.exports.isMainScopedRequire = function(node) {
-  if (!node) return false;
-
-  const c = node.callee;
-
-  return c &&
-         node.type === 'CallExpression' &&
-         c.type === 'MemberExpression' &&
-         c.object.type === 'MemberExpression' &&
-         c.object.object.type === 'Identifier' &&
-         c.object.object.name === 'require' &&
-         c.object.property.type === 'Identifier' &&
-         c.object.property.name === 'main' &&
-         c.property.type === 'Identifier' &&
-         c.property.name === 'require';
-};
-
-// Whether or not the node represents a require at the top of the module
-// Instead of trying to find the require then backtrack to the top,
-// just take the root and check its immediate child
-module.exports.isTopLevelRequire = function(node) {
-  if (node.type !== 'Program' || !node.body ||
-      node.body.length === 0 || !node.body[0].expression) {
-    return false;
+    return c &&
+           node.type === 'CallExpression' &&
+           c.type === 'Identifier' &&
+           c.name === 'define';
   }
 
-  return this.isRequire(node.body[0].expression);
-};
+  // Whether or not the node represents any of the AMD define() forms
+  isDefineAMD(node) {
+    if (!node) return false;
 
-// Whether or not the node represents an AMD-style driver script's require
-// Example: require(deps, function)
-module.exports.isAMDDriverScriptRequire = function(node) {
-  return this.isRequire(node) &&
-         node.arguments &&
-         node.arguments[0] && node.arguments[0].type &&
-         node.arguments[0].type === 'ArrayExpression';
-};
-
-function isExportsIdentifier(obj) {
-  return obj.type && obj.type === 'Identifier' && obj.name === 'exports';
-}
-
-function isModuleIdentifier(obj) {
-  return obj.type && obj.type === 'Identifier' && obj.name === 'module';
-}
-
-function isFunctionLike(node) {
-  if (!node) return false;
-
-  return node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression';
-}
-
-// module.exports.foo
-function isModuleExportsAttach(node) {
-  if (!node.object || !node.object.object || !node.object.property) return false;
-
-  return node.type === 'MemberExpression' &&
-         isModuleIdentifier(node.object.object) &&
-         isExportsIdentifier(node.object.property);
-}
-
-// module.exports
-function isModuleExportsAssign(node) {
-  if (!node.object || !node.property) return false;
-
-  return node.type === 'MemberExpression' &&
-         isModuleIdentifier(node.object) &&
-         isExportsIdentifier(node.property);
-}
-
-// exports
-function isExportsAssign(node) {
-  return isExportsIdentifier(node);
-}
-
-// exports.foo
-function isExportsAttach(node) {
-  return node.type === 'MemberExpression' && isExportsIdentifier(node.object);
-}
-
-// Whether or not the node represents the use of
-// assigning (and possibly attaching) something to module.exports or exports
-module.exports.isExports = function(node) {
-  if (node.type !== 'AssignmentExpression') return;
-
-  // Only the left side matters
-  const leftNode = node.left;
-
-  return isModuleExportsAttach(leftNode) || isModuleExportsAssign(leftNode) ||
-         isExportsAttach(leftNode) || isExportsAssign(leftNode);
-};
-
-// define('name', [deps], func)
-module.exports.isNamedForm = function(node) {
-  if (!this.isDefine(node)) return false;
-
-  const args = node.arguments;
-
-  return args && args.length === 3 &&
-         (args[0].type === 'Literal' || args[0].type === 'StringLiteral') &&
-         args[1].type === 'ArrayExpression' &&
-         isFunctionLike(args[2]);
-};
-
-// define([deps], func)
-module.exports.isDependencyForm = function(node) {
-  if (!this.isDefine(node)) return false;
-
-  const args = node.arguments;
-
-  return args && args.length === 2 &&
-         args[0].type === 'ArrayExpression' &&
-         isFunctionLike(args[1]);
-};
-
-// define(func(require))
-module.exports.isFactoryForm = function(node) {
-  if (!this.isDefine(node)) return false;
-
-  const args = node.arguments;
-  const firstParamNode = args.length > 0 && args[0].params ? args[0].params[0] : null;
-
-  // Node should have a function whose first param is 'require'
-  return args && args.length === 1 &&
-         isFunctionLike(args[0]) &&
-         firstParamNode && firstParamNode.type === 'Identifier' &&
-         firstParamNode.name === 'require';
-};
-
-// define({})
-module.exports.isNoDependencyForm = function(node) {
-  if (!this.isDefine(node)) return false;
-
-  const args = node.arguments;
-
-  return args && args.length === 1 && args[0].type === 'ObjectExpression';
-};
-
-// define(function(require, exports, module)
-module.exports.isREMForm = function(node) {
-  if (!this.isDefine(node)) return false;
-
-  const args = node.arguments;
-  const params = args.length > 0 ? args[0].params : null;
-
-  if (!args || args.length === 0 || !isFunctionLike(args[0]) || params.length !== 3) {
-    return false;
+    return this.isNamedForm(node) || this.isDependencyForm(node) ||
+           this.isFactoryForm(node) || this.isNoDependencyForm(node) ||
+           this.isREMForm(node);
   }
 
-  const [first, second, third] = params;
+  // Whether or not the node represents a require function call
+  isRequire(node) {
+    return this.isPlainRequire(node) || this.isMainScopedRequire(node);
+  }
 
-  return first.type === 'Identifier' && first.name === 'require' &&
-         second.type === 'Identifier' && second.name === 'exports' &&
-         third.type === 'Identifier' && third.name === 'module';
-};
+  // Whether or not the node represents a plain require function call [require(...)]
+  isPlainRequire(node) {
+    if (!node) return false;
 
-module.exports.isES6Import = function(node) {
-  switch (node.type) {
-    case 'Import':
-    case 'ImportDeclaration':
-    case 'ImportDefaultSpecifier':
-    case 'ImportNamespaceSpecifier': {
-      return true;
-    }
+    const c = node.callee;
 
-    default: {
+    return c &&
+      node.type === 'CallExpression' &&
+      c.type === 'Identifier' &&
+      c.name === 'require';
+  }
+
+  // Whether or not the node represents main-scoped require function call [require.main.require(...)]
+  isMainScopedRequire(node) {
+    if (!node) return false;
+
+    const c = node.callee;
+
+    return c &&
+      node.type === 'CallExpression' &&
+      c.type === 'MemberExpression' &&
+      c.object.type === 'MemberExpression' &&
+      c.object.object.type === 'Identifier' &&
+      c.object.object.name === 'require' &&
+      c.object.property.type === 'Identifier' &&
+      c.object.property.name === 'main' &&
+      c.property.type === 'Identifier' &&
+      c.property.name === 'require';
+  }
+
+  // Whether or not the node represents a require at the top of the module
+  // Instead of trying to find the require then backtrack to the top,
+  // just take the root and check its immediate child
+  isTopLevelRequire(node) {
+    if (node.type !== 'Program' || !node.body || node.body.length === 0 || !node.body[0].expression) {
       return false;
     }
+
+    return this.isRequire(node.body[0].expression);
   }
-};
 
-module.exports.isES6Export = function(node) {
-  switch (node.type) {
-    case 'ExportDeclaration':
-    case 'ExportNamedDeclaration':
-    case 'ExportSpecifier':
-    case 'ExportDefaultDeclaration':
-    case 'ExportAllDeclaration': {
-      return true;
-    }
+  // Whether or not the node represents an AMD-style driver script's require
+  // Example: require(deps, function)
+  isAMDDriverScriptRequire(node) {
+    return this.isRequire(node) &&
+      node.arguments &&
+      node.arguments[0] && node.arguments[0].type &&
+      node.arguments[0].type === 'ArrayExpression';
+  }
 
-    default: {
+  // Whether or not the node represents the use of
+  // assigning (and possibly attaching) something to module.exports or exports
+  isExports(node) {
+    if (node.type !== 'AssignmentExpression') return;
+
+    // Only the left side matters
+    const leftNode = node.left;
+
+    return this.#isModuleExportsAttach(leftNode) || this.#isModuleExportsAssign(leftNode) ||
+      this.#isExportsAttach(leftNode) || this.#isExportsAssign(leftNode);
+  }
+
+  // define('name', [deps], func)
+  isNamedForm(node) {
+    if (!this.isDefine(node)) return false;
+
+    const args = node.arguments;
+
+    return args && args.length === 3 &&
+           (args[0].type === 'Literal' || args[0].type === 'StringLiteral') &&
+           args[1].type === 'ArrayExpression' &&
+           this.#isFunctionLike(args[2]);
+  }
+
+  // define([deps], func)
+  isDependencyForm(node) {
+    if (!this.isDefine(node)) return false;
+
+    const args = node.arguments;
+
+    return args && args.length === 2 &&
+           args[0].type === 'ArrayExpression' &&
+           this.#isFunctionLike(args[1]);
+  }
+
+  // define(func(require))
+  isFactoryForm(node) {
+    if (!this.isDefine(node)) return false;
+
+    const args = node.arguments;
+    const firstParamNode = args.length > 0 && args[0].params ? args[0].params[0] : null;
+
+    // Node should have a function whose first param is 'require'
+    return args && args.length === 1 &&
+           this.#isFunctionLike(args[0]) &&
+           firstParamNode && firstParamNode.type === 'Identifier' &&
+           firstParamNode.name === 'require';
+  }
+
+  // define({})
+  isNoDependencyForm(node) {
+    if (!this.isDefine(node)) return false;
+
+    const args = node.arguments;
+
+    return args && args.length === 1 && args[0].type === 'ObjectExpression';
+  }
+
+  // define(function(require, exports, module)
+  isREMForm(node) {
+    if (!this.isDefine(node)) return false;
+
+    const args = node.arguments;
+    const params = args.length > 0 ? args[0].params : null;
+
+    if (!args || args.length === 0 || !this.#isFunctionLike(args[0]) || params.length !== 3) {
       return false;
     }
-  }
-};
 
-module.exports.isDynamicImport = function(node) {
-  return node.callee && node.callee.type === 'Import' && node.arguments.length > 0;
-};
+    const [first, second, third] = params;
+
+    return first.type === 'Identifier' && first.name === 'require' &&
+           second.type === 'Identifier' && second.name === 'exports' &&
+           third.type === 'Identifier' && third.name === 'module';
+  }
+
+  isES6Import(node) {
+    switch (node.type) {
+      case 'Import':
+      case 'ImportDeclaration':
+      case 'ImportDefaultSpecifier':
+      case 'ImportNamespaceSpecifier': {
+        return true;
+      }
+
+      default: {
+        return false;
+      }
+    }
+  }
+
+  isES6Export(node) {
+    switch (node.type) {
+      case 'ExportDeclaration':
+      case 'ExportNamedDeclaration':
+      case 'ExportSpecifier':
+      case 'ExportDefaultDeclaration':
+      case 'ExportAllDeclaration': {
+        return true;
+      }
+
+      default: {
+        return false;
+      }
+    }
+  }
+
+  isDynamicImport(node) {
+    return node.callee && node.callee.type === 'Import' && node.arguments.length > 0;
+  }
+
+  #isExportsIdentifier(obj) {
+    return obj.type && obj.type === 'Identifier' && obj.name === 'exports';
+  }
+
+  #isModuleIdentifier(obj) {
+    return obj.type && obj.type === 'Identifier' && obj.name === 'module';
+  }
+
+  // module.exports.foo
+  #isModuleExportsAttach(node) {
+    if (!node.object || !node.object.object || !node.object.property) return false;
+
+    return node.type === 'MemberExpression' &&
+           this.#isModuleIdentifier(node.object.object) &&
+           this.#isExportsIdentifier(node.object.property);
+  }
+
+  // module.exports
+  #isModuleExportsAssign(node) {
+    if (!node.object || !node.property) return false;
+
+    return node.type === 'MemberExpression' &&
+           this.#isModuleIdentifier(node.object) &&
+           this.#isExportsIdentifier(node.property);
+  }
+
+  // exports
+  #isExportsAssign(node) {
+    return this.#isExportsIdentifier(node);
+  }
+
+  // exports.foo
+  #isExportsAttach(node) {
+    return node.type === 'MemberExpression' && this.#isExportsIdentifier(node.object);
+  }
+
+  #isFunctionLike(node) {
+    if (!node) return false;
+
+    return node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression';
+  }
+}
+
+module.exports = new AstModuleTypes();
