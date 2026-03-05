c98b0ae527eb047dacbc68cee233b30ed671884c
XhmikosR <xhmikosr@gmail.com>
2026-03-05 08:32:45 +0200

    Cache repeated property reads and add early exits

--- index.js
+++ index.js
@@ -34,31 +34,32 @@ module.exports.isRequire = function(node) {
 // Whether or not the node represents a plain require function call [require(...)]
 module.exports.isPlainRequire = function(node) {
   if (!node) return false;
+  if (node.type !== 'CallExpression') return false;
 
   const c = node.callee;
 
-  return c &&
-    node.type === 'CallExpression' &&
-    c.type === 'Identifier' &&
-    c.name === 'require';
+  return c && c.type === 'Identifier' && c.name === 'require';
 };
 
 // Whether or not the node represents main-scoped require function call [require.main.require(...)]
 module.exports.isMainScopedRequire = function(node) {
   if (!node) return false;
+  if (node.type !== 'CallExpression') return false;
 
   const c = node.callee;
+  if (!c || c.type !== 'MemberExpression') return false;
 
-  return c &&
-    node.type === 'CallExpression' &&
-    c.type === 'MemberExpression' &&
-    c.object.type === 'MemberExpression' &&
-    c.object.object.type === 'Identifier' &&
-    c.object.object.name === 'require' &&
-    c.object.property.type === 'Identifier' &&
-    c.object.property.name === 'main' &&
-    c.property.type === 'Identifier' &&
-    c.property.name === 'require';
+  const obj = c.object;
+  if (!obj || obj.type !== 'MemberExpression') return false;
+
+  const base = obj.object;
+  const prop = obj.property;
+  if (!base || !prop) return false;
+
+  if (base.type !== 'Identifier' || base.name !== 'require') return false;
+  if (prop.type !== 'Identifier' || prop.name !== 'main') return false;
+
+  return c.property.type === 'Identifier' && c.property.name === 'require';
 };
 
 // Whether or not the node represents a require at the top of the module
@@ -76,42 +77,45 @@ module.exports.isTopLevelRequire = function(node) {
 // Whether or not the node represents an AMD-style driver script's require
 // Example: require(deps, function)
 module.exports.isAMDDriverScriptRequire = function(node) {
-  return this.isRequire(node) &&
-    node.arguments &&
-    node.arguments[0] && node.arguments[0].type &&
-    node.arguments[0].type === 'ArrayExpression';
+  if (!this.isRequire(node)) return false;
+
+  const args = node.arguments;
+  if (!args || args.length === 0) return false;
+
+  const firstArg = args[0];
+
+  return firstArg && firstArg.type === 'ArrayExpression';
 };
 
 function isExportsIdentifier(obj) {
-  return obj.type && obj.type === 'Identifier' && obj.name === 'exports';
+  return obj && obj.type === 'Identifier' && obj.name === 'exports';
 }
 
 function isModuleIdentifier(obj) {
-  return obj.type && obj.type === 'Identifier' && obj.name === 'module';
+  return obj && obj.type === 'Identifier' && obj.name === 'module';
 }
 
 function isFunctionLike(node) {
   if (!node) return false;
-
   return node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression';
 }
 
 // module.exports.foo
 function isModuleExportsAttach(node) {
-  if (!node.object || !node.object.object || !node.object.property) return false;
+  if (!node || node.type !== 'MemberExpression') return false;
 
-  return node.type === 'MemberExpression' &&
-    isModuleIdentifier(node.object.object) &&
-    isExportsIdentifier(node.object.property);
+  const obj = node.object;
+  if (!obj || obj.type !== 'MemberExpression') return false;
+
+  return isModuleIdentifier(obj.object) && isExportsIdentifier(obj.property);
 }
 
 // module.exports
 function isModuleExportsAssign(node) {
   if (!node.object || !node.property) return false;
+  if (node.type !== 'MemberExpression') return false;
 
-  return node.type === 'MemberExpression' &&
-    isModuleIdentifier(node.object) &&
-    isExportsIdentifier(node.property);
+  return isModuleIdentifier(node.object) && isExportsIdentifier(node.property);
 }
 
 // exports
@@ -121,13 +125,14 @@ function isExportsAssign(node) {
 
 // exports.foo
 function isExportsAttach(node) {
+  if (!node) return false;
   return node.type === 'MemberExpression' && isExportsIdentifier(node.object);
 }
 
 // Whether or not the node represents the use of
 // assigning (and possibly attaching) something to module.exports or exports
 module.exports.isExports = function(node) {
-  if (node.type !== 'AssignmentExpression') return;
+  if (!node || node.type !== 'AssignmentExpression') return false;
 
   // Only the left side matters
   const leftNode = node.left;
@@ -141,11 +146,14 @@ module.exports.isNamedForm = function(node) {
   if (!this.isDefine(node)) return false;
 
   const args = node.arguments;
+  if (!args || args.length !== 3) return false;
 
-  return args && args.length === 3 &&
-    (args[0].type === 'Literal' || args[0].type === 'StringLiteral') &&
-    args[1].type === 'ArrayExpression' &&
-    isFunctionLike(args[2]);
+  const [first, second, third] = args;
+  const firstArgType = first.type;
+
+  if (firstArgType !== 'Literal' && firstArgType !== 'StringLiteral') return false;
+
+  return second.type === 'ArrayExpression' && isFunctionLike(third);
 };
 
 // define([deps], func)
@@ -153,10 +161,9 @@ module.exports.isDependencyForm = function(node) {
   if (!this.isDefine(node)) return false;
 
   const args = node.arguments;
+  if (!args || args.length !== 2) return false;
 
-  return args && args.length === 2 &&
-    args[0].type === 'ArrayExpression' &&
-    isFunctionLike(args[1]);
+  return args[0].type === 'ArrayExpression' && isFunctionLike(args[1]);
 };
 
 // define(func(require))
@@ -164,13 +171,16 @@ module.exports.isFactoryForm = function(node) {
   if (!this.isDefine(node)) return false;
 
   const args = node.arguments;
-  const firstParamNode = args.length > 0 && args[0].params ? args[0].params[0] : null;
+  if (!args || args.length !== 1) return false;
+
+  const firstArg = args[0];
+  if (!isFunctionLike(firstArg)) return false;
+
+  const firstParamNode = firstArg.params && firstArg.params[0];
+  if (!firstParamNode) return false;
 
   // Node should have a function whose first param is 'require'
-  return args && args.length === 1 &&
-    isFunctionLike(args[0]) &&
-    firstParamNode && firstParamNode.type === 'Identifier' &&
-    firstParamNode.name === 'require';
+  return firstParamNode.type === 'Identifier' && firstParamNode.name === 'require';
 };
 
 // define({})
@@ -178,8 +188,9 @@ module.exports.isNoDependencyForm = function(node) {
   if (!this.isDefine(node)) return false;
 
   const args = node.arguments;
+  if (!args || args.length !== 1) return false;
 
-  return args && args.length === 1 && args[0].type === 'ObjectExpression';
+  return args[0].type === 'ObjectExpression';
 };
 
 // define(function(require, exports, module)
@@ -187,13 +198,13 @@ module.exports.isREMForm = function(node) {
   if (!this.isDefine(node)) return false;
 
   const args = node.arguments;
-  const params = args.length > 0 ? args[0].params : null;
+  if (!args || args.length === 0) return false;
 
-  if (!args || args.length === 0 || !isFunctionLike(args[0]) || params.length !== 3) {
-    return false;
-  }
+  const firstArg = args[0];
+  if (!isFunctionLike(firstArg)) return false;
+  if (!firstArg.params || firstArg.params.length !== 3) return false;
 
-  const [first, second, third] = params;
+  const [first, second, third] = firstArg.params;
 
   return first.type === 'Identifier' && first.name === 'require' &&
     second.type === 'Identifier' && second.name === 'exports' &&
@@ -201,36 +212,25 @@ module.exports.isREMForm = function(node) {
 };
 
 module.exports.isES6Import = function(node) {
-  switch (node.type) {
-    case 'Import':
-    case 'ImportDeclaration':
-    case 'ImportDefaultSpecifier':
-    case 'ImportNamespaceSpecifier': {
-      return true;
-    }
+  const t = node.type;
 
-    default: {
-      return false;
-    }
-  }
+  return t === 'Import' ||
+    t === 'ImportDeclaration' ||
+    t === 'ImportDefaultSpecifier' ||
+    t === 'ImportNamespaceSpecifier';
 };
 
 module.exports.isES6Export = function(node) {
-  switch (node.type) {
-    case 'ExportDeclaration':
-    case 'ExportNamedDeclaration':
-    case 'ExportSpecifier':
-    case 'ExportDefaultDeclaration':
-    case 'ExportAllDeclaration': {
-      return true;
-    }
+  const t = node.type;
 
-    default: {
-      return false;
-    }
-  }
+  return t === 'ExportDeclaration' ||
+    t === 'ExportNamedDeclaration' ||
+    t === 'ExportSpecifier' ||
+    t === 'ExportDefaultDeclaration' ||
+    t === 'ExportAllDeclaration';
 };
 
 module.exports.isDynamicImport = function(node) {
-  return node.callee && node.callee.type === 'Import' && node.arguments.length > 0;
+  const c = node.callee;
+  return c && c.type === 'Import' && node.arguments.length > 0;
 };
